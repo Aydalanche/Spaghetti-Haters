18858c1585e71109914c0dd9c106467eb8d06b7d
Aydalanche <Aydalanche@gmail.com>
2022-08-27 16:22:34 -0700

    Put links into a different background file... JS has me coping

--- popup.js
+++ popup.js
@@ -1,7 +1,36 @@
-// popup.js
+var checkPageButton = document.getElementById('clickIt');
+var theWow = document.getElementById('theWow');
+
+function getSwitch() {
+  chrome.storage.local.get(['mySwitch'], result => {
+    checkPageButton.style.backgroundColor = result.mySwitch ? "green" : "red";
+    checkPageButton.style.borderColor = result.mySwitch ? "green" : "red";
+    checkPageButton.innerHTML = result.mySwitch ? "Enabled" : "Disabled";
+    theWow.innerHTML = result.mySwitch ? "Click down below to disable blocking": "Click down below to enable blocking";
+  });
+}
+
+getSwitch();
+
 document.addEventListener('DOMContentLoaded', function() {
-  var checkButton = document.getElementById('check');
-  checkButton.addEventListener('click', function() {
-   alert("Hey your button is working!");
+  checkPageButton.addEventListener('click', function() {
+
+    chrome.storage.local.get(['mySwitch'], result => {
+      let changeSwitch = !result.mySwitch;
+      if (changeSwitch == false) {
+        chrome.storage.local.set({mySwitch: false}, () => {
+          getSwitch();
+        });
+      } else {
+        chrome.storage.local.set({mySwitch: true}, () => {
+          getSwitch();
+      });
+    }});
+
+    chrome.runtime.sendMessage({show: "refresh"});
+
+    chrome.tabs.getSelected(null, function(tab) {
+      chrome.tabs.reload()
+    });
   }, false);
 }, false);
