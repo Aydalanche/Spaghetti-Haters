b4556cd785b740810f591dd0c9d9abcf3e83fab5
Aydalanche <Aydalanche@gmail.com>
2022-08-31 14:19:20 -0700

    We still hate spaghetti

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 var checkPageButton = document.getElementById('clickIt');
 var theWow = document.getElementById('theWow');
+var refreshButton = document.getElementById('clickIt2');
 
 function getSwitch() {
   chrome.storage.local.get(['mySwitch'], result => {
@@ -13,8 +14,14 @@ function getSwitch() {
 getSwitch();
 
 document.addEventListener('DOMContentLoaded', function() {
+  //Refresh Button
+  refreshButton.addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function (arrayOfTabs) {
+      chrome.tabs.reload(arrayOfTabs[0].id);
+    });
+  });
+  // On Off Button
   checkPageButton.addEventListener('click', function() {
-
     chrome.storage.local.get(['mySwitch'], result => {
       let changeSwitch = !result.mySwitch;
       if (changeSwitch == false) {
@@ -25,12 +32,10 @@ document.addEventListener('DOMContentLoaded', function() {
         chrome.storage.local.set({mySwitch: true}, () => {
           getSwitch();
       });
+      
     }});
-
-    chrome.runtime.sendMessage({show: "refresh"});
-
-    chrome.tabs.getSelected(null, function(tab) {
-      chrome.tabs.reload()
-    });
+    chrome.tabs.query({active: true, currentWindow: true}, function (arrayOfTabs) {
+      chrome.tabs.reload(arrayOfTabs[0].id);
+  });
   }, false);
 }, false);
